3989837a10e2c31055151be50189c12c9e8261c9
gchavez25-star <gchavez@inewton.edu.pe>
2026-04-01 18:20:17 -0500

    01042026

--- src/components/ui/Campus.jsx
+++ src/components/ui/Campus.jsx
@@ -29,7 +29,7 @@ const CampusSection = () => {
     {
       name: "Campus Los Baños del Inca",
       location: "Zona natural",
-      image: "/Inicio/Campus/Baños.png",
+      image: "/Campus/Campus/LocalBDI.jpg",
       description:
         "Entorno natural ideal para el aprendizaje experiencial y actividades al aire libre.",
       icon: <Trees className="w-6 h-6 text-azul-oscuro" />,
